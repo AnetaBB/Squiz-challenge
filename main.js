880a0f0c15058ed07c6ea4f37e6d31bb65715fe8
Aneta <a.banas.brejt@gmail.com>
2021-12-08 23:25:24 +0100

    Display retrived data as a list

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
-// import './CSS/style.css';
-/* function fetchData() {
-  fetch('https://dujour.squiz.cloud/developer-challenge/data')
-    .then((response) =>
-      console.log(
-        'response:',
-        response.json().then((json) => console.log(json))
-      )
-    )
-    .catch((error) => console.log('error:', error));
-}
-
-fetchData();
- */
+const listItems = {};
 const getData = async () => {
   const response = await fetch(
     'https://dujour.squiz.cloud/developer-challenge/data'
   );
   const data = await response.json();
-  console.log('data:', data);
+
+  listItems.data = data;
+  return data;
 };
-getData();
-const element = document.getElementById('data');
-element.innerHTML = '<div>jakiś inny tekst</div>';
-console.log(element.innerHTML);
+
+const makeList = async () => {
+  const data = await getData();
+  console.log('data:', data);
+  var html = '<ul>';
+  data.map((item) => {
+    html =
+      html +
+      `<li key=${item.id}>
+      name: ${item.name},&nbsp;
+      country: ${item.country},&nbsp;
+      industry: ${item.industry},&nbsp;
+      numberOfEmployees: ${item.numberOfEmployees}
+      </li>`;
+  });
+  var html = html + '</ul>';
+  updateDOM('data', html);
+};
+makeList();
+
+const updateDOM = (id, html) => {
+  const element = document.getElementById(id);
+  element.innerHTML = html;
+};
